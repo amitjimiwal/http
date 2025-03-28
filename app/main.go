e7f23f4690836bc6eb1c54533ea2307e8ac66638
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2025-03-28 08:59:57 +0530

    [upd]

--- app/main.go
+++ app/main.go
@@ -13,9 +13,9 @@ var _ = os.Exit
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-	l,err :=net.Listen("tcp","0.0.0.0:3000");
+	l,err :=net.Listen("tcp","0.0.0.0:4221");
 	if err!=nil{
-		fmt.Println("Failed to bind to port 3000");
+		fmt.Println("Failed to bind to port 4221");
 		os.Exit(1);
 	}
 	_,err=l.Accept()
